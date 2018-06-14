47dc6be7bc380e7bd089ba30b019cbab1c719b12
rjgc <rjgc@qq.com>
2018-06-14 11:07:04 +0800

    测试

---
+++ src/main/java/com/j5erp/J5erpApplication.java
@@ -0,0 +1,12 @@
+package com.j5erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J5erpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J5erpApplication.class, args);
+    }
+}
