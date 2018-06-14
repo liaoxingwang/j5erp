e2c352846fc8bd445fbab581d045ad86335732cc
rjgcadmin <rjgcadmin@163.com>
2018-06-14 17:13:20 +0800

    第一次提交项目刘江

---
+++ src/main/java/com/j5erp/service/serviceimpl/AdvanceDetailServiceImpl.java
@@ -0,0 +1,6 @@
+package com.j5erp.service.serviceimpl;
+
+import com.j5erp.service.AdvanceDetailService;
+
+public class AdvanceDetailServiceImpl implements AdvanceDetailService {
+}
---
+++ src/main/java/com/j5erp/service/serviceimpl/BalanceAdvancedetailServiceImpl.java
@@ -0,0 +1,7 @@
+package com.j5erp.service.serviceimpl;
+
+import com.j5erp.service.BalanceAdvancedetailService;
+import com.j5erp.service.BalanceDetailService;
+
+public class BalanceAdvancedetailServiceImpl implements BalanceAdvancedetailService {
+}
