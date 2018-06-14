8790a2dcfce35080b2e7a7c8ecfb13cb498b055e
rjgc <rjgc@qq.com>
2018-06-14 20:32:01 +0800

    github

--- src/main/java/com/j5erp/J5erpApplication.java
+++ src/main/java/com/j5erp/J5erpApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class J5erpApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(J5erpApplication.class, args);
     }
 }
