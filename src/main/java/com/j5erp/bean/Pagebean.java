6292abeabdc2a3ff28bde1651f2dd8dd25b475ad
kamis <kamis@163.com>
2018-06-15 12:44:08 +0800

    pagebean1

--- src/main/java/com/j5erp/bean/Pagebean.java
+++ src/main/java/com/j5erp/bean/Pagebean.java
@@ -1,4 +1,26 @@
 package com.j5erp.bean;
 
-public class Pagebean {
+import java.util.List;
+
+public class Pagebean <E>{
+    private Integer currentPage;
+    private Integer prePage;
+    private Integer nextPage;
+    private Integer totalPage;
+    private Integer totalCount;
+    private Integer pageSize;
+    private List<E> list;
+
+    public Pagebean() {
+    }
+
+    public Pagebean(Integer currentPage, Integer totalCount, Integer pageSize, List<E> list) {
+        this.currentPage = currentPage;
+        this.totalCount = totalCount;
+        this.pageSize = pageSize;
+        this.list = list;
+        this.totalPage = totalCount%pageSize==0?totalCount/pageSize:(totalCount/pageSize)+1;
+        this.prePage = currentPage>1?currentPage-1:1;
+        this.nextPage = currentPage<this.totalPage?currentPage+1:this.totalPage;
+    }
 }
