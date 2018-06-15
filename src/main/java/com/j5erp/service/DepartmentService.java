caf494d3fae22f1944ad96d1f9aa904b987498fe
rjgcadmin <rjgcadmin@163.com>
2018-06-15 11:51:06 +0800

    第三次提交项目潘啸

--- src/main/java/com/j5erp/service/DepartmentService.java
+++ src/main/java/com/j5erp/service/DepartmentService.java
@@ -1,5 +1,8 @@
 package com.j5erp.service;
 
+import com.j5erp.entity.Department;
+
 public interface DepartmentService {
     //部门
+    void insertdepar(Department department);
 }
--- src/main/java/com/j5erp/service/serviceimpl/DepartmentServiceImpl.java
+++ src/main/java/com/j5erp/service/serviceimpl/DepartmentServiceImpl.java
@@ -1,7 +1,18 @@
 package com.j5erp.service.serviceimpl;
 
+import com.j5erp.entity.Department;
+import com.j5erp.mapper.DepartmentMapper;
 import com.j5erp.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DepartmentServiceImpl implements DepartmentService {
     //部门
+    @Autowired
+    private DepartmentMapper departmentmapper;
+
+    public void insertdepar(Department department){
+        departmentmapper.insertdepar(department);
+    }
 }
